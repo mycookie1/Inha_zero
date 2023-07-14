80c5bcc9a8e2c38db7e331b304cabf5b4dd028fc
Cookie <youbellgun31@gmail.com>
2023-07-15 00:37:30 +0900

    [feat] 데이버테이스변경

--- server/neordinary-template-springboot-java-server/src/main/java/com/example/demo/src/user/UserService.java
+++ server/neordinary-template-springboot-java-server/src/main/java/com/example/demo/src/user/UserService.java
@@ -12,6 +12,7 @@ import com.example.demo.src.user.DTO.*;
 import com.example.demo.src.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -20,9 +21,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
@@ -82,77 +81,6 @@ public class UserService {
 
     }
 //    public String getReceipt(String url1) throws BaseException {
-//
-////        try {
-////            URL url = new URL(url1);
-////            String postData = "foo1=bar1&foo2=bar2";
-////            Images images = new Images();
-////            images.setUrl(url1);
-////            requestBody.put("images", images);
-////        requestBody.put("requestId", "string");
-////        requestBody.put("resultType", "string");
-////        requestBody.put("timestamp", "");
-////        requestBody.put("version", "V1");
-////
-////
-////            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-////            conn.setRequestMethod("POST");
-////            conn.setDoOutput(true);
-////            conn.setRequestProperty("Content-Type", "application/json");
-////            conn.setRequestProperty("X-OCR-SECRET", Secret.CLOVA_SECRET_KEY);
-////            conn.setUseCaches(false);
-////
-////            try (DataOutputStream dos = new DataOutputStream(conn.getOutputStream())) {
-////                dos.writeBytes(postData);
-////            }
-////
-////            try (BufferedReader br = new BufferedReader(new InputStreamReader(
-////                    conn.getInputStream())))
-////            {
-////                String line;
-////                while ((line = br.readLine()) != null) {
-////                    System.out.println(line);
-////                }
-////            }
-////        } catch (Exception e) {
-////            e.printStackTrace();
-////        }
-//
-//
-//        String ENDPOINT = "https://eqkyhw38bo.apigw.ntruss.com/custom/v1/23707/5503778efc7906255bde5996d820ed721e318f1873624979d4f420eab2c5ae91/general";
-//        Images images = new Images();
-//        images.setUrl(url1);
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//        //TODO: 시크릿키 등록
-//        headers.set("X-OCR-SECRET", Secret.CLOVA_SECRET_KEY);
-//
-//        Map<String, Object> requestBody = new HashMap<>();
-//
-//
-//        // Message: model, role
-//        requestBody.put("images", images);
-//        requestBody.put("requestId", "string");
-//        requestBody.put("resultType", "string");
-//        requestBody.put("timestamp", "");
-//        requestBody.put("version", "V1");
-//
-//        try {
-//            HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(requestBody, headers);
-//
-//            RestTemplate restTemplate = new RestTemplate();
-//            System.out.println("sjfslkdjfsjdslkjf");
-//            ResponseEntity<Map> response = restTemplate.postForEntity(ENDPOINT, requestEntity, Map.class);
-//            System.out.println("jjfkdjkfdkfkdkfjdkjf");
-//            // answer 추출
-//            System.out.println(response.getBody());
-//            //String[] responses = response.getBody().get("choices").toString().split("role=assistant, content=|}, finish_reason=stop, index=0}");
-//            //GetOpenAIReqDto getOpenAIResDto = new GetOpenAIReqDto(responses[1]);
-//            return "test";
-//        }
-//        catch (Exception e){
-//            throw new BaseException(BaseResponseStatus.OPENAI_ERROR);
-//        }
 //
 //    }
 }
